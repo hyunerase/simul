01cb98e7601fd1235df0446dad19ea731e612f4a
hyunerase <162708793+hyunerase@users.noreply.github.com>
2024-11-14 08:51:50 +0900

    Delete accounts directory

--- accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-
-class UserPro(AbstractUser):
-    access_token = models.CharField(max_length=225, blank=True, null=True)
-    nickname = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='images/', null=True, blank=True)
--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from flan.urls import path
-from . import views
-
-app_name = 'accounts'
-
-urlpatterns = [
-    path("kakao/login/", views.kakao_login, name="kakao_login"),
-    path("kakao/callback/", views.kakao_callback, name="kakao_callback"),
-]
